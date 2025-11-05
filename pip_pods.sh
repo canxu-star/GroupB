e4b1e48db5e1de8ecd62f06597df914a9754c7c8
LoveFlowers793 <yifan@whaletech.ai>
2025-11-05 15:04:14 +0800

    all

--- pip_pods.sh
+++ pip_pods.sh
@@ -3,6 +3,6 @@ pip install flash-attn --no-build-isolation
 pip install datasets
 pip install hf-transfer
 
-huggingface-cli download LoveFlowers793/MouseMDM \
+huggingface-cli download GSAI-ML/LLaDA-8B-Instruct \
   --local-dir ./model \
   --local-dir-use-symlinks False
